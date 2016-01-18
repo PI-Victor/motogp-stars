9ba806e07f276c14b5ccbd490a7792b14fd10b6d
Victor Palade <palade.ionut@gmail.com>
2016-01-18 20:52:58 +0100

    added fixtures for all the motogp events
    Signed-off-by: pi-victor <ipalade@redhat.com>

--- app.js
+++ app.js
@@ -8,19 +8,42 @@ App.Store = DS.Store.extend({
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend()
 
+/* Riders information
+ Riders also have ranks which directly affect the outcome of their event.
+ Pratice, Warm up, Qualifying, etc.
+ */
 App.Rider = DS.Model.extend({
   RiderRank: DS.attr('int'),
   RiderName: DS.attr('string')
 })
 
+/* Events can be:
+  Pratice, (Standard rules apply to practice, first 12 go to Q2 and the rest to Q1)
+  Qualifying 1, (First two places go to Q2)
+  Qualifying 2, (First one goes to pole and that gives him a starting advantage)
+  Warm up,
+  Race
+*/
 App.RideEvents = DS.Model.extend({
-  EventName: DS.atr('string')
+  EventName: DS.atrr('string')
 })
 
-App.MogoGPBike = DS.Model.extend({
+/*
+Motogp bikes also have ranking that are mixed with rider ranking to influence the
+outcome of events.
+*/
+App.MotoGPBike = DS.Model.extend({
   BikeName: DS.attr('string')
 })
 
+
+/* Circuits have weather as a variable, that would affect rider and bike
+performance.
+*/
 App.MotoGPCircuit = DS.Model.extend({
   Name: DS.attr('string')
 })
+
+function loadFixtures(){
+
+}

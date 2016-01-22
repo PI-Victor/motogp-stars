088109e2c788df675e91f4e67320076be4b1bae1
Victor Palade <palade.ionut@gmail.com>
2016-01-22 13:41:52 +0100

    moved json to js config objects modified structure of bike and team config
    Signed-off-by: pi-victor <ipalade@redhat.com>

---
+++ config/application.js
@@ -0,0 +1,9 @@
+export config = []
+
+config.events = [
+  {index: 1, eventIndex: 1, name: 'Practice'},
+  {index: 2, eventIndex: 2, name: 'Qualifying 1'},
+  {index: 3, eventIndex: 3, name: 'Qualifying 2'},
+  {index:4, eventIndex: 4, name: 'Warmp Up'},
+  {index:5, eventIndex: 5, name: 'Race'}
+]
---
+++ config/riders.js
@@ -0,0 +1,160 @@
+var riders = []
+
+riders = [
+  {
+    team_index: 6,
+    rank: 7.7,
+    racing_number: '08',
+    name: 'Hector Barbera',
+    champion: false,
+    injured: false
+  },
+  {
+    team_index: 6,
+    rank: 7.6,
+    racing_number: '76',
+    name: 'Loris Baz',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.2,
+    racing_number: '19',
+    name: 'Alvaro Bautista',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.5,
+    racing_number: '06',
+    name: 'Stefan Bradl',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 8.6,
+    racing_number: '35',
+    name: 'Cal Crutchlow',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 8.8,
+    racing_number: '04',
+    name: 'Andreea Dovizioso',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.8,
+    racing_number: '41',
+    name: 'Aleix Espargaro',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 8.3,
+    racing_number: '44',
+    name: 'Pol Espargaro',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.0,
+    racing_number: '68',
+    name: 'Yonni Hernandez',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 8.7,
+    racing_number: '29',
+    name: 'Andreea Iannone',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 6.9,
+    racing_number: '50',
+    name: 'Eugene Laverty',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 9.8,
+    racing_number: '99',
+    name: 'Jorge Lorenzo',
+    champion: true,
+    injured: false
+  },
+  {
+    rank: 9.8,
+    racing_number: '93',
+    name: 'Marc Marquez',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.0,
+    racing_number: '43',
+    name: 'Jack Miller',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 9.3,
+    racing_number: '26',
+    name: 'Dani Pedrosa',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.2,
+    racing_number: '09',
+    name: 'Danilo Petrucci',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.0,
+    racing_number: '53',
+    name: 'Esteve Rabat',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.1,
+    racing_number: '45',
+    name: 'Scott Redding',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 9.5,
+    racing_number: '46',
+    name: 'Valentino Rossi',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 8.3,
+    racing_number: '38',
+    name: 'Bradley Smith',
+    champion: false,
+    injured: false
+  },
+  {
+    rank: 7.1,
+    racing_number: '25',
+    name: 'Maverick Vinales',
+    champion: false,
+    injured: false
+  }
+
+
+
+
+
+
+
+]

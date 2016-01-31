bda9352f063406fdc5c31147b4168e6fb65e56d7
Victor Palade <palade.ionut@gmail.com>
2016-01-31 03:35:55 +0100

    added more teams to the team config
    Signed-off-by: pi-victor <ipalade@redhat.com>

--- config/teams.js
+++ config/teams.js
@@ -1,43 +1,45 @@
-var teams = []
-
-teams = [
-  {index: 1, name: 'Ducati MotoGP', rank: 8.5,
-    type: 'factory', engines: 5,
-    prototype: {name: 'Desmosedici GP15', rank: 8.7}
-  },
-  {index: 2, name: 'Team Suzuki MotoGP', rank: 7.9,
-    type: 'factory', engines: 5,
-    prototype: {name: 'GSX-RR', rank: 8.0}
-  },
-  {index: 3, name: 'Repsol Honda MotoGP', rank: 9.8,
-    type: 'factory', engines: 5,
-    prototype: {name: 'RC213V', rank: 9.8}
-  },
-  {index: 4, name: 'Aprilia Racing Team Gresini', rank: 9.8,
-    type: 'factory', engines: 5,
-    prototype: {name: 'RS-GP', rank: 6.7}
-  },
-  {index: 5, name: 'Movistar Yamaha MotoGP', rank: 9.8,
-    type: 'factory', engines: 5,
-    prototype: {name: 'YZRM1', rank: 9.8}
-  },
-  {index: 6, name: 'Avintia Racing', rank: 7.0,
-    type: 'open', engines: 12,
-    prototype: {name: 'Desmosedici GP14.2', rank: 7.0}
-  },
-  {index: 7, name: 'Octo Pramac Racing', rank: 7.8,
-    type: 'satellite', engines: 5,
-    prototype: {name: 'Desmosedici GP14.2', rank: 7.0}
-  },
-  {index: 8, name: 'E-Motion IodaRacing Team', rank: 6.9, type: 3},
-  {index: 9, name: 'LCR Honda', rank: 7.2, type: 2},
-  {index: 10, name: 'Monster Yamaha Tech 3', rank: 8.1, type: 2},
-  {index: 11, name: 'Aspar MotoGP Team', rank: 7.3, type: 3},
-  {index: 12, name: 'EG 0.0 Marc VDS', rank:7.1, type: 3}
-]
-
-teams.types = [
-  {index: 1, name: 'factory', engines: 5},
-  {index: 2, name: 'satellite', engines: 5},
-  {index: 3, name: 'open', engines: 12},
-]
+module "config" {
+  export default function() {
+    var teams = [
+      {index: 1, name: 'Ducati MotoGP', rank: 8.5,
+        type: 'factory', engines: 5,
+        prototype: {name: 'Desmosedici GP15', rank: 8.7}
+      },
+      {index: 2, name: 'Team Suzuki MotoGP', rank: 7.9,
+        type: 'factory', engines: 5,
+        prototype: {name: 'GSX-RR', rank: 8.0}
+      },
+      {index: 3, name: 'Repsol Honda MotoGP', rank: 9.8,
+        type: 'factory', engines: 5,
+        prototype: {name: 'RC213V', rank: 9.8}
+      },
+      {index: 4, name: 'Aprilia Racing Team Gresini', rank: 9.8,
+        type: 'factory', engines: 5,
+        prototype: {name: 'RS-GP', rank: 6.7}
+      },
+      {index: 5, name: 'Movistar Yamaha MotoGP', rank: 9.8,
+        type: 'factory', engines: 5,
+        prototype: {name: 'YZRM1', rank: 9.8}
+      },
+      {index: 6, name: 'Avintia Racing', rank: 7.0,
+        type: 'open', engines: 12,
+        prototype: {name: 'Desmosedici GP14.2', rank: 7.0}
+      },
+      {index: 7, name: 'Octo Pramac Racing', rank: 7.8,
+        type: 'satellite', engines: 5,
+        prototype: {name: 'Desmosedici GP14.2', rank: 7.0}
+      },
+      {index: 8, name: 'E-Motion IodaRacing Team', rank: 6.9, type: 3},
+      {index: 9, name: 'LCR Honda', rank: 7.2, type: 2},
+      {index: 10, name: 'Monster Yamaha Tech 3', rank: 8.1, type: 2},
+      {index: 11, name: 'Aspar MotoGP Team', rank: 7.3, type: 3},
+      {index: 12, name: 'EG 0.0 Marc VDS', rank:7.1, type: 3}
+    ]
+    return teams
+  }
+}
+// teams.types = [
+//   {index: 1, name: 'factory', engines: 5},
+//   {index: 2, name: 'satellite', engines: 5},
+//   {index: 3, name: 'open', engines: 12},
+// ]
